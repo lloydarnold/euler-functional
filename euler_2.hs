d433551ba6cc0b8d1023aaff94a7e308178b6ef6
lloarn <lloarn@penguin>
2020-03-24 18:35:19 +0000

    Add solution to euler_2

---
+++ euler_2.hs
@@ -0,0 +1,8 @@
+problem_2 :: Int
+problem_2 = sum (filter even (filter (<4000000) (firstNFib 100000)))
+
+fibs :: [Integer]
+fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
+
+firstNFib :: Int -> [Integer]
+firstNFib n = take n fibs
