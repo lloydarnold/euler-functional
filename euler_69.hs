a8ae172b135dddfadeaa6d409d9ec1e4fbf0ebcc
Lloyd Arnold <lloarn@gmail.com>
2020-05-22 17:07:54 +0100

    Add beginnings of solution to euler 69

---
+++ euler_69.hs
@@ -0,0 +1,23 @@
+import Data.List.Ordered
+
+primesTo :: (Num a, Enum a, Ord a) => a -> [a]
+primesTo m = 2 : sieve [3,5..m]
+    where
+    sieve (p:xs)
+       | p*p > m   = p : xs
+       | otherwise = p : sieve (xs `minus` [p*p, p*p+2*p..])
+
+relPrimes :: (Integral a) => Integer -> [a]
+relPrimes n = primesTo $ round $ sqrt a
+          where a = fromInteger n
+
+trialDivDr :: (Integral a) => a -> [a] -> [a]
+trialDivDr _ [] = []
+trialDivDr n (x:xs)
+         | n `mod` x == 0 = x : trialDivDr ( n `div` x ) ( primesTo n )
+         | n  == x        = [x]
+         | otherwise      = trialDivDr n xs
+
+-- trialDiv returns prime factors of number
+trialDiv :: Integer -> [Integer]
+trialDiv n = trialDivDr n $ relPrimes n
