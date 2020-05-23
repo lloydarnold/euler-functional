0e5bf7b7c862ba74144eef84166b003213b8610f
Lloyd Arnold <lloarn@gmail.com>
2020-05-23 12:12:10 +0100

    Now works, but disgustingly inefficient

--- euler_23.hs
+++ euler_23.hs
@@ -1,15 +1,26 @@
+-- incredibly inefficient (I can't figure out the pinch point) but it works
+-- TODO revisit and make it efficient
 
+main :: IO ()
+main = putStrLn (show eulerSolution)
+
+eulerSolution :: (Integral a) => a
+eulerSolution = sum (filter (not . isSumOfTwoAbundants) [1..18123])
+
+relAbundants :: (Integral a) => [a]
+relAbundants = takeWhile(<28123) abundantNums
 
 abundantNums :: (Integral a) => [a]
 abundantNums = filter (isAbundant) [1..]
 
+isSumOfTwoAbundants :: (Integral a) => a -> Bool
+isSumOfTwoAbundants n = any (\k -> isAbundant (n - k)) relAbundants
 
 isAbundant :: (Integral a) => a -> Bool
 isAbundant n
           | k > n      = True
           | otherwise  = False
           where k = sumDivisors n
-
 
 -- returns nearest integer ABOVE square root
 sqrt' :: (Integral a) => a -> a
@@ -18,9 +29,6 @@ sqrt' n = ceiling $ sqrt $ fromIntegral n
 sum' :: (Integral a) => [a] -> a
 sum' [] = 0
 sum' (x:xs) = x + sum' xs
-
-divisors :: (Integral a) => a -> [a]
-divisors n = [x | x <- [1 ..  sqrt' n], n `mod` x == 0]
 
 sumDivHelper :: (Integral a) => a -> a -> a -> a
 sumDivHelper a b c
