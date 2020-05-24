7702100ae60e96aa98ee9a69e4938e9dfc072357
Lloyd Arnold <lloarn@gmail.com>
2020-05-24 19:59:54 +0100

    Solution now works, but not in good time

--- euler_35.hs
+++ euler_35.hs
@@ -1,21 +1,45 @@
+import Data.List.Ordered
+import Data.Array
+
+main :: IO ()
+main = print( eulerSolution )
+
+eulerSolution :: (Integral a) => a
+eulerSolution = length' 0 $ filter (circP) $ primesTo 1000000
+
 circP :: (Integral a) => a -> Bool
-circP n = True
+circP n = circPWorker n $ nextRot n
 
 circPWorker :: (Integral a) => a -> a -> Bool
 circPWorker n g
-          | n == g        = True
-          | (isPrime g)   = False
-          | otherwise     = circPWorker n $ nextRot g
+          | n == g            = True
+          | not (isPrime g)   = False
+          | otherwise         = circPWorker n $ nextRot g
 
 nextRot :: (Integral a) => a -> a
-nextRot n = revListify n
+nextRot n = listToNum $ nrWorker $ listify n
 
 nrWorker :: (Integral a) => [a] -> [a]
 nrWorker (x:xs) = xs ++ [x]
 
-revListToNum :: (Integral a) => [a] -> a -> a
-revListToNum [] n = n
-revListToNum (x:xs) n = revListToNum xs $ n + x * ( powers 10 ( length xs ) )
+primesTo :: (Num a, Enum a, Ord a) => a -> [a]
+primesTo m = 2 : sieve [3,5..m]
+    where
+    sieve (p:xs)
+       | p*p > m   = p : xs
+       | otherwise = p : sieve (xs `minus` [p*p, p*p+2*p..])
+
+
+listToNumHlpr :: (Integral a) => [a] -> a -> a
+listToNumHlpr [] n = n
+listToNumHlpr (x:xs) n = listToNumHlpr xs $ n + x * ( powers 10 ( length' 0 xs) )
+
+listToNum :: (Integral a) => [a] -> a
+listToNum xs = listToNumHlpr xs 0
+
+length' :: (Integral a) => a -> [a] -> a
+length' a []     = a
+length' a (x:xs) = length' (a+1) xs
 
 powers :: (Integral a) => a -> a -> a
 powers 0 _ = 0
@@ -28,8 +52,26 @@ revListify :: (Integral a) => a -> [a]
 revListify 0 = []
 revListify n = n `mod` 10 : revListify (n `div` 10)
 
+listify :: (Integral a) => a -> [a]
+listify n = reverse $ revListify n
+
+primeState :: Array Integer Integer
+primeState = listArray (0,1000000) [arrayPopulate n | n <- [1..1000000]]
+
+arrayPopulate :: (Integral a, Num p) => a -> p
+arrayPopulate n
+          | isPrimeInit n = 0
+          | otherwise = 1
+
 isPrime :: (Integral a) => a -> Bool
-isPrime k = if k > 1 then null [ x | x <- [2..isqrt k], k `mod` x == 0] else False
+isPrime n = integralToBool $ primeState ! ( fromIntegral n )
+
+integralToBool :: (Integral a) => a -> Bool
+integralToBool 0 = False
+integralToBool 1 = True
+
+isPrimeInit :: (Integral a) => a -> Bool
+isPrimeInit k = if k > 1 then null [ x | x <- [2..isqrt k], k `mod` x == 0] else False
 
 -- returns nearest integer ABOVE square root
 isqrt :: (Integral a) => a -> a
