fd9d57e9ed8022a0650efa5181530abdf8a3d7a5
Lloyd Arnold <lloarn@gmail.com>
2020-05-26 13:14:05 +0100

    Add naive brute force solution to euler 71
    Can (should) be optimised with Farey sequence

---
+++ euler_71.hs
@@ -0,0 +1,24 @@
+import Data.Ratio
+
+limit :: Integer
+limit = 1000000
+
+eulerSolution :: Ratio Integer
+eulerSolution = ordFracs !! ( ( length $ takeWhile (< 3%7) ordFracs )- 1)
+
+ordFracs :: [Ratio Integer]
+ordFracs = quickSort listFracs
+
+listFracs :: [Ratio Integer]
+listFracs = [n % d | d <- [1..limit], n <- [1..d], hcf d n == 1]
+
+hcf :: Integer -> Integer -> Integer
+hcf a 0 = a
+hcf a b = hcf b (a `mod` b)
+
+quickSort :: (Ord a) => [a] -> [a]
+quickSort [] = []
+quickSort (x:xs) =
+    let smallerSorted = quickSort (filter (<=x) xs)
+        biggerSorted = quickSort (filter (>x) xs)
+    in  smallerSorted ++ [x] ++ biggerSorted
