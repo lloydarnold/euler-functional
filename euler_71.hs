7e23780adafd8e280650317432d5ab38880b36b7
Lloyd Arnold <lloarn@gmail.com>
2020-05-27 12:15:00 +0100

    Fully code up speedy nifty & elegant solution

--- euler_71.hs
+++ euler_71.hs
@@ -4,8 +4,21 @@ import Data.Ratio
 Specifically, uses the property that each new term is the mediant of the
 previous adjacant pair --}
 
+target :: Ratio Integer
+target = 3%7
 limit :: Integer
 limit = 1000000
 
 mediant :: Ratio Integer -> Ratio Integer -> Ratio Integer
 mediant a b = (numerator a + numerator b) % ( denominator a + denominator b)
+
+eulerSolution :: Ratio Integer
+eulerSolution = driver (0%1) (1%1) (0%1)
+
+driver :: Ratio Integer -> Ratio Integer -> Ratio Integer -> Ratio Integer
+driver l r p
+        | a > target  = driver l a a
+        | a == target = driver l a p
+        | otherwise   = if denominator a < limit then driver a r a
+                                                 else p
+        where a = mediant l r
