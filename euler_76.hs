03936b7b003a8e8971103e44a2afdb589b56c7be
Lloyd Arnold <lloarn@gmail.com>
2020-05-27 21:33:02 +0100

    Add start of euler 76 solution

---
+++ euler_76.hs
@@ -0,0 +1,3 @@
+{-- Fundamentally, this is a combinatorics problem (and almost identical to
+    one I was asked in my Ox interview how wild). I did much of the intellectual
+    legwork on paper before coming here.  --}
